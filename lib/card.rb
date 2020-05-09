fbb37ad0f4f1132c1330503aa6d39de789e63bb8
Amy Moller <amy.moller@yahoo.com>
2020-05-09 17:02:48 -0600

    Delete unnecessary fluff

--- lib/card.rb
+++ lib/card.rb
@@ -11,19 +11,4 @@ class Card
   end
 
 
-  def add_suit
-    @suit = suit
-    add_suit = ['clubs', 'hearts', 'diamonds', 'spades']
-  end
-
-  def add_value
-    @value = value
-    add_value = ['jack', 'queen', 'king', 'ace']
-  end
-
-  def add_rank
-    @rank = rank
-    add_rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-  end
-
 end
