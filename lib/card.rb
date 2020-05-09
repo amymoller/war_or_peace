9af7c0266f78526aae81a0ece2d20d4ae278ec50
Amy Moller <amy.moller@yahoo.com>
2020-05-09 15:06:03 -0600

    Clean up card values

--- lib/card.rb
+++ lib/card.rb
@@ -10,19 +10,20 @@ class Card
     @rank = rank
   end
 
-end
 
   def add_suit
     @suit = suit
-    add_suit = ["clubs", "hearts", "diamonds", "spades"]
+    add_suit = ['clubs', 'hearts', 'diamonds', 'spades']
   end
 
   def add_value
     @value = value
-    add_value = ["jack", "queen", "king", "ace"]
+    add_value = ['jack', 'queen', 'king', 'ace']
   end
 
   def add_rank
     @rank = rank
-    add_rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]
+    add_rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
   end
+
+end
