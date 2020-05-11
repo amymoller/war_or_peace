9d50fdaa276ba445e85dd5f6c4dbab0e85d548f0
Amy Moller <amy.moller@yahoo.com>
2020-05-10 21:09:37 -0600

    Add all testing deck methods

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,4 +4,47 @@ require './lib/deck'
 
 class DeckTest < Minitest::Test
   def setup
-    
+    deck = Deck.new(cards)
+  end
+
+  def test_it_exists
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
+  end
+
+  def test_it_has_cards
+    deck = Deck.new(cards)
+
+  end
+
+  def test_rank_of_card_at
+    deck = Deck.new(cards)
+
+
+  end
+
+  def test_it_has_high_ranking_cards
+    deck = Deck.new(cards)
+
+
+  end
+
+  def it_has_a_percentage_of_high_ranking_cards
+    deck = Deck.new(cards)
+
+  end
+
+  def test_it_can_remove_top_card
+    deck = Deck.new(cards)
+
+
+  end
+
+  def test_it_can_add_card_to_bottom
+    deck = Deck.new(cards)
+
+
+  end
+
+end
